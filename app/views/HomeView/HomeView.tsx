56f3ee625c5c54da99f5411f697fc8d2e2d68bcc
Hoang Tran <hoangtranxamk@gmail.com>
2023-02-11 18:53:59 +0200

    Initial setup

---
+++ app/views/HomeView/HomeView.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, View, Text } from 'react-native';
+
+export const HomeView = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>HomeViews</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
