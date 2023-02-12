5da5b3f26c5ef484b2b990c8753a543a86ae1ee9
Hoang Tran <hoangtranxamk@gmail.com>
2023-02-12 14:51:22 +0200

    Add color and styling

--- app/components/CatCard/CatCard.tsx
+++ app/components/CatCard/CatCard.tsx
@@ -1,4 +1,12 @@
-import { Image, StyleSheet, View, ViewStyle, Button } from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  View,
+  ViewStyle,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+import { colors } from '../../Constants/Colors';
 import { Spacer } from '../Spacer/Spacer';
 
 interface CardProps {
@@ -12,8 +20,13 @@ export const CatCard = ({ imageUrl, containerStyle, onPress }: CardProps) => {
     <View style={[containerStyle, styles.container]}>
       <Spacer height={64} />
       <Image source={{ uri: imageUrl }} style={styles.image} />
-      <Spacer height={32} />
-      <Button title={'Dislike'} onPress={onPress} />
+      <Spacer height={16} />
+      <TouchableOpacity
+        onPress={onPress}
+        activeOpacity={0.8}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Dislike</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -21,11 +34,23 @@ export const CatCard = ({ imageUrl, containerStyle, onPress }: CardProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   image: {
     aspectRatio: 1,
     width: '100%',
     borderRadius: 10,
+  },
+  button: {
+    padding: 16,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: colors.keppelGreen,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '700',
+    color: colors.keppelGreen,
   },
 });
--- app/views/HomeView/HomeView.tsx
+++ app/views/HomeView/HomeView.tsx
@@ -1,7 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useState, useEffect } from 'react';
-import { SafeAreaView, StyleSheet, Text, Button } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { HorizontalCatList } from '../../components/HorizontalCatList/HorizontalCatList';
+import { colors } from '../../Constants/Colors';
 import {
   FakeCat,
   generateFakeCatArray,
@@ -66,7 +67,12 @@ export const HomeView = () => {
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Please Rescue the cats!</Text>
       <HorizontalCatList data={catData} onCardPress={onCardPress} />
-      <Button title="Clear dislike data" onPress={clearAsyncStorage} />
+      <TouchableOpacity
+        onPress={clearAsyncStorage}
+        activeOpacity={0.8}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Clear dislike data</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -76,5 +82,15 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: '700',
     textAlign: 'center',
+    color: colors.keppelGreen,
+  },
+  button: {
+    padding: 8,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '700',
+    color: colors.engineeringOrange,
   },
 });
